e1bc727472d315fb5abfce798d8a5307a5e88558
Colin Clark <cclark@ccs.neu.edu>
2015-12-14 17:21:26 -0500

    Add paragraph/newline handling, more output config, import/export

--- main.py
+++ main.py
@@ -1,43 +1,84 @@
 #! /usr/bin/env python
 
 import argparse
+import cPickle
 import nodebuilders
 import random
+import re
 import util
 
 parser = argparse.ArgumentParser(description="An AI for writing prose \"similar\" to an input document.")
 parser.add_argument('data', type=argparse.FileType('r'), help="The file to read training data from")
 parser.add_argument('output',type=argparse.FileType('w'), help="The file to write the novel to")
+
+parser.add_argument('-x', '--export', action='store_true', help="Export training data to the output file instead of writing anything.")
+parser.add_argument('-i', '--import', action='store_true', dest='imp', help="Import pre-calculated training data from the input file, instead of parsing prose")
+
+parser.add_argument('-c', '--word-count', type=int, default=2000, metavar="WC",
+        help="The minimum number of words to write. Will quit writing after finishing a sentence.")
+parser.add_argument('-w', '--wrap-width', type=int, default=150, metavar="WW",
+        help="The number of characters to wrap the text to. Single words longer than this may still go past this amount.")
 args = parser.parse_args()
 
-training_map = util.DefaultDict(util.Counter)
-nodebuilder = nodebuilders.SentenceBuilder()
+nodebuilder = nodebuilders.ParagraphBuilder()
+if args.imp:
+    training_map = cPickle.load(args.data)
+else:
+    training_map = util.DefaultDict(util.Counter)
 
-last_node = None
-for line in args.data:
-    for word in line.split():
-        node = nodebuilder.buildNode(word, last_node)
-        if last_node is not None:
-            training_map[last_node][node] += 1
-        last_node = node
+    not_title = re.compile('.*[a-z].*')
 
-for tran in training_map.values():
-    tran.normalize()
+    last_node = None
+    blank_lines = 0
+    for line in args.data:
+        words = line.split()
 
-start_nodes = [k for k in training_map if k[1]]
+        is_title = False
+        if not words:
+            blank_lines += 1
+        elif not not_title.match(line):
+            is_title = True
 
-word_count = 0
-current_node = random.choice(start_nodes)
-with args.output as out:
+        for word in words:
+            node = nodebuilder.buildNode(word, last_node, blank_lines, is_title)
+            blank_lines = 0;
+            if last_node is not None:
+                training_map[last_node][node] += 1
+            last_node = node
+args.data.close()
+
+if args.export:
+    cPickle.dump(training_map, args.output, -1)
+else:
+    start_nodes = [k for k in training_map if k[1]]
+
+    word_count = 0
+    line_width = 0
+    current_node = random.choice(start_nodes)
+
     while True:
-        out.write(nodebuilder.getWord(current_node))
-        word_count += 1
+        word = nodebuilder.getWord(current_node)
 
-        if word_count > 1000 and current_node[2]:
+        if current_node[3] > 0:
+            args.output.write('\n' + '\n'*current_node[3])
+            line_width = 0
+        elif line_width > 0 and line_width+len(word)+1 < args.wrap_width:
+            args.output.write(' ')
+            line_width += 1
+        elif line_width > 0:
+            args.output.write('\n')
+            line_width = 0
+
+        args.output.write(word)
+        word_count += 1
+        line_width += len(word)
+
+        if word_count > args.word_count and current_node[2]:
             break
-        out.write(' ')
+
         current_node = training_map[current_node].sample()
 
         if current_node is None:
             print "Oh noes!"
-            exit()
+            break
+args.output.close()
--- nodebuilders.py
+++ nodebuilders.py
@@ -25,3 +25,22 @@ class SentenceBuilder(object):
 
     def getWord(self, node):
         return node[0]
+
+
+class ParagraphBuilder(object):
+    def __init__(self):
+        # A word is considered to end a sentence if it ends in one of . ? !
+        # optionally followed by some number of single or double quotes
+        self.endPattern = re.compile('.*[.!?][\'"]*')
+
+    def buildNode(self, word, last_node, lines_before, is_title):
+
+        # This word is considered to start a sentence if it is the first word
+        # or the last word ended a sentence
+        startsSentence = True if last_node is None else last_node[2]
+        endsSentence = self.endPattern.match(word) is not None
+
+        return (word, startsSentence, endsSentence, lines_before, is_title)
+
+    def getWord(self, node):
+        return node[0]

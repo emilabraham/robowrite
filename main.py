0fb6ae37a35dba2a1ac13fcdfbc8d6166dd8fdbb
Emil Abraham <emilabraham1414@gmail.com>
2015-12-14 20:58:36 -0500

    Unfinished quotes

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ parser.add_argument('output',type=argparse.FileType('w'), help="The file to writ
 args = parser.parse_args()
 
 training_map = util.DefaultDict(util.Counter)
+training_map_quote_length = util.DefaultDict(util.Counter)
 nodebuilder = nodebuilders.SentenceBuilder()
 
 last_node = None
@@ -21,23 +22,92 @@ for line in args.data:
             training_map[last_node][node] += 1
         last_node = node
 
-for tran in training_map.values():
-    tran.normalize()
+start_quote_nodes = [k for k in training_map if k[3]]
 
+#start quote stack
+startStack = []
 
 start_nodes = [k for k in training_map if k[1]]
+#start_quote_nodes = [k for k in training_map if k[3]]
+end_quote_nodes = [k for k in training_map if k[4]]
 
 word_count = 0
 current_node = random.choice(start_nodes)
+
 with args.output as out:
     while True:
+        current_node = training_map[current_node].sample()
+        word = nodebuilder.getWord(current_node)
+        resample_count = 0
+
+        #If we sample a startQuote and we have 2 quotes to close, resample
+        while current_node[3] and len(startStack) > 1:
+            current_node = training_map[current_node].sample()
+            #update word
+            word = nodebuilder.getWord(current_node)
+            resample_count += 1
+            if resample_count > 10:
+                print "Too much resampling. I'm stuck. Help."
+                break
+
+        #If we sample an endQuote and there are no quotes to close, resample
+        while current_node[4] and len(startStack) is 0:
+            current_node = training_map[current_node].sample()
+            #update word
+            word = nodebuilder.getWord(current_node)
+            resample_count += 1
+            if resample_count > 10:
+                print "Too much resampling. I'm stuck. Help."
+                break
+
+        #If the current_node starts a quote, append the quotes onto stack
+        if current_node[3] and len(startStack) is 0:
+            if word[0] is ('"', "'"):
+                startStack.append(word[0])
+            if word[1] in  ('"', "'"):
+                startStack.append(word[1])
+        #If current_node starts a quote and stack is only 1 long, append if it
+        #makes sense
+        elif current_node[3] and len(startStack) is 1:
+            #If you are not duplicating the stack and only single quote word
+            if word[0] is not startStack[0] and word[1] not in ('"', "'"):
+                startStack.append(word[0])
+
+        #If we sample an endQuote and we have quotes to close
+        if current_node[4] and len(startStack) > 0:
+            #Only 1 quote to close
+            if len(startStack) is 1:
+                #If it only closes the open quote on stack, clear the stack
+                if word[-1:] is startStack[0] and word[-2:-1] not in ('"', "'"):
+                    startStack.pop()
+                #Resample otherwise
+                else:
+                    current_node = training_map[current_node].sample()
+                    word = nodebuilder.getWord(current_node)
+
+            #2 quotes to close
+            else:
+                #If it closes both quotes, clear the stack
+                if word[-2:] is "'\"":
+                    startStack.pop()
+                    startStack.pop()
+                #Remove last element if single, inner quote. Only double-quote
+                #should remain
+                elif word[-1:] is "'":
+                    startStack.pop()
+                #Resample otherwise
+                else:
+                    current_node = training_map[current_node].sample()
+                    word = nodebuilder.getWord(current_node)
+
         out.write(nodebuilder.getWord(current_node))
         word_count += 1
 
         if word_count > 1000 and current_node[2]:
             break
         out.write(' ')
-        current_node = training_map[current_node].sample()
+        
+        
 
         if current_node is None:
             print "Oh noes!"
--- nodebuilders.py
+++ nodebuilders.py
@@ -15,10 +15,14 @@ class SentenceBuilder(object):
         self.endPattern = re.compile('.*[.!?][\'"]*')
 
         #Matches word starting with single or double quotes
-        self.startsQuote = re.compile('[\'"].*[^.!?]')
+        self.startsQuote = re.compile('[\'"].*')
 
         #Matches for punctuation and then any type of quote at end of word
-        self.endsQuote = re.compile('[-.!?,][\'"]$')
+        self.endsQuote = re.compile('.*[\'"]+$')
+
+        #Stack of quotes that need to be closed
+        self.startStack = []
+        self.endStack = []
 
     def buildNode(self, word, last_node):
 
@@ -29,7 +33,20 @@ class SentenceBuilder(object):
         startsQuote = self.startsQuote.match(word) is not None
 
         #Search anywhere in word for the endsQuote pattern
-        endsQuote = self.endsQuote.search(word) is not None
+        endsQuote = self.endsQuote.match(word) is not None
+
+        #Append any opening quotes to startStack
+        if startsQuote:
+            if word[0] in ('"', "'"):
+                self.startStack.append(word[0])
+            if word[1] in ('"', "'"):
+                self.startStack.append(word[1])
+
+        if endsQuote:
+            if word[-1:] in ('"', "'"):
+                self.endStack.append(word[-1:])
+            if word[-2:-1] in ('"', "'"):
+                self.endStack.append(word[-2:-1])
 
         return (word, startsSentence, endsSentence, startsQuote, endsQuote)
 

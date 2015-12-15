74b626ecec6ddd11b4b4b04c685bff5eb45b6b4e
Colin Clark <cclark@ccs.neu.edu>
2015-12-14 21:55:04 -0500

    Added basic quote handling

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ parser.add_argument('-w', '--wrap-width', type=int, default=150, metavar="WW",
         help="The number of characters to wrap the text to. Single words longer than this may still go past this amount.")
 args = parser.parse_args()
 
-nodebuilder = nodebuilders.ParagraphBuilder()
+nodebuilder = nodebuilders.QuoteBuilder()
 if args.imp:
     training_map = cPickle.load(args.data)
 else:
@@ -54,10 +54,17 @@ else:
 
     word_count = 0
     line_width = 0
+    in_quote = False
     current_node = random.choice(start_nodes)
+
+    trimQuotes = re.compile('[^\'"]+[\'"]*$')
 
     while True:
         word = nodebuilder.getWord(current_node)
+
+        if in_quote and current_node[5]:
+            trimmed = trimQuotes.search(word)
+            word = trimmed.group() if trimmed else word
 
         if current_node[3] > 0:
             args.output.write('\n' + '\n'*current_node[3])
@@ -76,9 +83,25 @@ else:
         if word_count > args.word_count and current_node[2]:
             break
 
-        current_node = training_map[current_node].sample()
+        if current_node[5]:
+            in_quote = True
+
+        if current_node[6]:
+            in_quote = False
+
+
+        if in_quote:
+            possible_nodes = [n for n in training_map[current_node] if not n[5]]
+
+            if not possible_nodes:
+                current_node = training_map[current_node].sample()
+            else:
+                current_node = random.choice(possible_nodes)
+        else:
+            current_node = training_map[current_node].sample()
 
         if current_node is None:
-            print "Oh noes!"
-            break
+            print "Failed to find a node. Picking randomly..."
+            current_node = random.choice(start_nodes)
+
 args.output.close()
--- nodebuilders.py
+++ nodebuilders.py
@@ -44,3 +44,34 @@ class ParagraphBuilder(object):
 
     def getWord(self, node):
         return node[0]
+
+
+
+class QuoteBuilder(object):
+    def __init__(self):
+        # A word is considered to end a sentence if it ends in one of . ? !
+        # optionally followed by some number of single or double quotes
+        self.endPattern = re.compile('.*[.!?][\'"]*$')
+
+        self.startQuotePattern = re.compile('[\'"]+')
+        self.endQuotePattern = re.compile('[\'"]+$')
+
+    def buildNode(self, word, last_node, lines_before, is_title):
+
+        # This word is considered to start a sentence if it is the first word
+        # or the last word ended a sentence
+        startsSentence = True if last_node is None else last_node[2]
+        endsSentence = self.endPattern.match(word) is not None
+
+        startQuote = self.startQuotePattern.match(word)
+        endQuote = self.endQuotePattern.search(word)
+
+        if startQuote is not None:
+            startQuote = startQuote.group()
+        if endQuote is not None:
+            endQuote = endQuote.group()
+
+        return (word, startsSentence, endsSentence, lines_before, is_title, startQuote, endQuote)
+
+    def getWord(self, node):
+        return node[0]
